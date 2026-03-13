97d595ca26aac25cfaff4fd05bafa4d6921fd1a0
corebyte56 <fahim789m@gmail.com>
2026-03-13 20:32:17 +0600

    add: TypeScript string variable example

---
+++ TypeScript-Basic/Variable Types/string.ts
@@ -0,0 +1,8 @@
+// Declare a variable named 'name' with string type
+let name: string = 'Fahim';
+
+// Example of type error (number cannot be assigned to string)
+// let user: string = 6;
+
+// Print the value of name to the console
+console.log(name);
