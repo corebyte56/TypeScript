4a59ff3b59fd9e1ff6c5ca93066d0d330357f79f
corebyte56 <fahim789m@gmail.com>
2026-03-13 20:28:37 +0600

    add: TypeScript number variable example

---
+++ TypeScript-Basic/Variable Types/number.ts
@@ -0,0 +1,8 @@
+// Declare a variable named 'num' with number type
+let num: number = 23;
+
+// Example of type error (string cannot be assigned to number)
+// let number: number = 'alex';
+
+// Print the value of num to the console
+console.log(num);
