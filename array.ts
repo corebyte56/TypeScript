c8a72b49c690c73bfea42def05a49cc8834bd658
corebyte56 <fahim789m@gmail.com>
2026-03-13 20:44:33 +0600

    feat: add TypeScript array type example with forEach loop

---
+++ array.ts
@@ -0,0 +1,11 @@
+    // TypeScript Example
+    // In TypeScript we define the type of the array
+    let numbers: number[] = [1, 2, 3, 4, 5];
+
+    // In JavaScript we don't define the array type
+    // let numbers = [1, 2, 3, 4, 5];
+
+    // Loop through the array and print each number
+    numbers.forEach((num) => {
+    console.log(num);
+    });
