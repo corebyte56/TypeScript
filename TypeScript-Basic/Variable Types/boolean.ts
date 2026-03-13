600906568e65aca13df00cbc9b88b34a59ffe670
corebyte56 <fahim789m@gmail.com>
2026-03-13 20:35:04 +0600

    feat: add TypeScript boolean variable example

---
+++ TypeScript-Basic/Variable Types/boolean.ts
@@ -0,0 +1,8 @@
+// Declare a variable named 'isOpen' with boolean type
+let isOpen: boolean = true;
+
+// Example of type error (string cannot be assigned to boolean)
+// let isClose: boolean = 'alex';
+
+// Print the value of isOpen to the console
+console.log(isOpen);
